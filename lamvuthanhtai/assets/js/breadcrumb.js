1c79b0d8f2f0537f2acf53549ff044213c0dbe98
lamvuthanhtai02 <lamvuthanhtai14042002@gmail.com>
2023-08-12 22:08:44 +0700

    finish test

--- lamvuthanhtai/assets/js/breadcrumb.js
+++ lamvuthanhtai/assets/js/breadcrumb.js
@@ -4,3 +4,11 @@ const menuNavigation = document.querySelector('#menu__list');
 menuButton.addEventListener('click', () => {
     menuNavigation.classList.toggle('show');
 });
+
+window.addEventListener('wheel', () => {
+    menuNavigation.classList.remove('show');
+});
+
+window.addEventListener('touchmove', () => {
+    menuNavigation.classList.remove('show');
+});
